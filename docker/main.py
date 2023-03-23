74e02d770a7f53c5809a3b279abcf2cfca4b8e44
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-03-23 07:45:00 +0000

    update

--- docker/main.py
+++ docker/main.py
@@ -2,24 +2,24 @@
 # Inspired by the notebook on jane street competition
 
 # %%
+import zstd
+import boto3
+import pathlib
+import warnings
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.backend as K
+import tensorflow.keras.layers as layers
+from tensorflow.keras.callbacks import Callback, ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
+import asyncio
+import polars as pl
+import sys
+import subprocess
 import os
+import json
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
-import subprocess
-import sys
-import polars as pl
-import asyncio
-from tensorflow.keras.callbacks import Callback, ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
-import tensorflow.keras.layers as layers
-import tensorflow.keras.backend as K
-import tensorflow as tf
-import polars as pl
-import numpy as np
-import pandas as pd
-import warnings
-import pathlib
-import boto3
-import zstd
 
 tf.random.set_seed(42)
 os.mkdir("/tmp/input")
@@ -30,6 +30,7 @@ DATA_FORMAT = os.environ["DATA_FORMAT"]
 BUCKET = "compute-assets-36607b9c2d934caa8bf7d19e0251bab6"
 S3_CLIENT = boto3.client("s3")
 LABEL_SIZE = os.environ["LABEL_SIZE"]
+
 
 def create_ae_mlp(num_columns, num_labels, hidden_units, dropout_rates, ls=1e-2, lr=1e-3):
 
@@ -96,29 +97,44 @@ def prepare_model(side, df):
     if path.is_file():
         return tf.keras.models.load_model(path)
     else:
-        return create_ae_mlp(**params)
+        model = create_ae_mlp(**params)
+        model.evaluate
+        return model
 
 
 def load_data(format, data_path):
+    filename = pathlib.Path(data_path).name
     if format == "taker_positions":
+        suffix = filename.split("_")[4]
+        cols = list(map(lambda x: pl.col(x), json.load(
+            open("./synthetic_positions.json", "r"))))
+        cols.append(pl.col("timestamp"))
         lazy_frame = pl.scan_csv(data_path).with_columns(pl.col("timestamp").str.strptime(
-            pl.Datetime("ns"))).drop(["status", "order_book_id"]).sort("timestamp")
+            pl.Datetime("ns"))).drop(["include_time_range_name"]).select(cols).select([pl.col("timestamp"), pl.col("*").exclude("timestamp").map_alias(lambda x: x+"_"+suffix)])
     elif format == "order_depth":
+        suffix = filename.split("_")[3]
+        print('suffix: ', suffix)
         lazy_frame = pl.scan_parquet(data_path).with_columns(pl.col("timestamp").str.strptime(
-            pl.Datetime("ns"))).drop(["status", "order_book_id"]).sort("timestamp")
+            pl.Datetime("ns"))).drop(["order_book_id"]).select([pl.col("timestamp"), pl.col("*").exclude("timestamp").map_alias(lambda x: x+"_"+suffix)])
     elif format == "depth_imbalance":
-        lazy_frame = pl.scan_parquet(data_path).drop("order_book_id").sort("timestamp")
+        lazy_frame = pl.scan_parquet(data_path).drop("order_book_id")
     else:
         raise ""
 
-    return lazy_frame.select([pl.col("timestamp"), pl.col([pl.Float64, pl.Int64]).cast(pl.Float32)]).select(["timestamp", pl.col(pl.Float32)])
+    return lazy_frame.select([pl.col("timestamp"), pl.col([pl.Float64, pl.Int64]).cast(pl.Float32)]).select(["timestamp", pl.col(pl.Float32)]).sort("timestamp")
 
+def feature_engineering(lazy_frame: pl.LazyFrame):
+    lazy_frame.select([
+        pl.col(pl.Float32).rolling_mean("60s", by="timestamp"),
+        pl.col(pl.Float32).rolling_mean("15m", by="timestamp"),
+        pl.col(pl.Float32).rolling_mean("60m", by="timestamp"),
+    ])
 
-def load_label(data_df, label_path):
+def load_label(data_df: pl.DataFrame, label_path):
     df_label = pl.scan_csv(label_path).select(["signal", pl.col(
-        "base_timestamp").alias("timestamp").str.strptime(pl.Datetime("ns"))])
-    dflabel = df_label.join_asof(data_df.select("timestamp"), on="timestamp", strategy="forward").sort(
-        "timestamp").select([pl.col("signal")]).collect()
+        "base_timestamp").alias("timestamp").str.strptime(pl.Datetime("ns"))]).sort("timestamp")
+    dflabel = df_label.join_asof(data_df.select("timestamp").lazy(), on="timestamp", strategy="forward").sort(
+        "timestamp").collect()
     return dflabel
 
 
@@ -139,9 +155,10 @@ def train_model(side: str, df_X: pl.DataFrame, df_Y: pl.DataFrame, model=None):
         df_X_slice = df_X[prev:i]
         df_Y_slice = df_Y[prev:i]
         model.fit(df_X_slice.to_pandas(), df_Y_slice.to_pandas())
-        print("", flush = True)
+        print("", flush=True)
         prev = i
     return model
+
 
 def file_to_disk(key: str, filetype, body):
     filename = pathlib.Path(key).name
@@ -149,7 +166,7 @@ def file_to_disk(key: str, filetype, body):
         filepath = f"/tmp/input/data/{filename}"
     else:
         filepath = "/tmp/input/label.csv"
-    fp = open(filepath, "wb")
+    fp = open(filepath.replace(".csv.zst", ".csv"), "wb")
     if key.endswith("zst"):
         s = zstd.decompress(body.read())
     else:
@@ -157,6 +174,7 @@ def file_to_disk(key: str, filetype, body):
     fp.write(s)
     fp.flush()
     fp.close()
+
 
 def download_files(idx: str):
     print(os.listdir("/tmp/input/data"), flush=True)
@@ -182,17 +200,24 @@ def download_files(idx: str):
         obj = S3_CLIENT.get_object(Bucket=BUCKET, Key=i["key"])
         file_to_disk(i["key"], "label", obj["Body"])
 
+
 def process_files(buymodel, sellmodel):
-    stack = []
+    stack: list[pl.LazyFrame] = []
     for i in pathlib.Path("/tmp/input/data/").iterdir():
-        stack.append(load_data(DATA_FORMAT, i))
+        lf = load_data(DATA_FORMAT, i)
+        stack.append(lf)
     df = stack[0]
-    print(df.columns)
     for df2 in stack[1:]:
         df = df.join(df2, on="timestamp")
-    df_X = df.select(pl.col(pl.Float32)).collect()
-    print("all data loaded onto the memory: ", df_X.estimated_size(), flush=True)
-    df_Y = load_label(df, "/tmp/input/label.csv")
+    
+    df_X = df.select([pl.col("timestamp"), pl.col(pl.Float32)]).collect()
+    df_Y = load_label(df_X, "/tmp/input/label.csv")
+    if df_X.height != df_Y.height:
+        df_X = df_X.join_asof(df_Y, on="timestamp")
+    df_Y = df_Y.select("signal")
+    print(df_X)
+    print("all data loaded onto the memory: ",
+          df_X.estimated_size(), flush=True)
     print("label loaded onto the memory: ", df_X.estimated_size(), flush=True)
     buymodel = train_model("buy", df_X, df_Y, buymodel)
     sellmodel = train_model("sell", df_X, df_Y, sellmodel)
@@ -206,13 +231,15 @@ def main():
         buymodel, sellmodel = process_files(buymodel, sellmodel)
     buymodel.save("/tmp/output/buy")
     sellmodel.save("/tmp/output/sell")
-    suffix=f"{DATA_FORMAT}_{TARGET_PRODUCT}/{LABEL_SIZE}"
+    suffix = f"{DATA_FORMAT}_{TARGET_PRODUCT}/{LABEL_SIZE}"
+
     def upload_model(directory):
-        os.system(f"aws s3 cp /tmp/output/{directory} s3://{BUCKET}/model/{directory}/{suffix} --recursive")
+        os.system(
+            f"aws s3 cp /tmp/output/{directory} s3://{BUCKET}/model/{directory}/{suffix} --recursive")
     print(os.walk(f"/tmp/output/buy"))
     upload_model("buy")
     upload_model("sell")
-    
+
 
 print("starting")
 main()
